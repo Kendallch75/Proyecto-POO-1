e3c93238ae41dd5ccc05cc95f281cc19f2b09ee4
nahun-395 <166880408+nahun-395@users.noreply.github.com>
2025-10-20 17:29:15 -0600

    Create class Consultorio

---
+++ Consultorio.java
@@ -0,0 +1,40 @@
+package proye1;
+
+import java.util.Arrays;
+
+public class Consultorio {
+
+    protected int numero;
+    protected boolean[] reservaciones = new boolean[6]; // true = ocupado, false = desocupado
+
+    public Consultorio(int num) {
+        this.numero = num;
+    }
+    
+    public void getNombre() {
+        System.out.println("Consultorio Numero: " + numero);
+    }
+
+    public void setNombre(int num) {
+        this.numero = num;
+    }
+
+    public String getReservaciones() {
+        return Arrays.toString(reservaciones);
+    }
+
+    public void asignarConsultorio() {
+        for (int i = 0; i < reservaciones.length; i++) {
+            if (!reservaciones[i]) { // Si está desocupado (false)
+                reservaciones[i] = true; // Asignar cita (ocupar)
+                System.out.println("Consultorio reservado: " + numero);
+                return;
+            }
+        }
+        System.out.println("No hay consultorios disponibles");
+    }
+    
+    public void finUso() {
+        reservaciones[numero] = false;
+    }
+}
